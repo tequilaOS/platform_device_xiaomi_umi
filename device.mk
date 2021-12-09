55b2c8a9bfea941bc1c49ef66c8fa41af5d5d863
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-09 20:35:50 +0100

    lmi: Commonize popup camera stuff
    Change-Id: I40195b126b3f79566240f88f0b2bf55f941ccd66

--- device.mk
+++ device.mk
@@ -34,14 +34,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
-# Camera
-PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.motor@1.0.vendor
-
-# Device parts
-PRODUCT_PACKAGES += \
-    DeviceParts
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml
