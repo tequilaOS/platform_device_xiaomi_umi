ab0b8560ee8c812004ed44109fc07b4c1440ace9
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-10 14:57:10 +0100

    lmi: Update blobs from V12.5.3.0.RJKMIXM
    Change-Id: Id8807e9d5f8837f07a2babfc8ab58250cc32ec8b

--- libinit/init_xiaomi_lmi.cpp
+++ libinit/init_xiaomi_lmi.cpp
@@ -9,7 +9,7 @@
 
 #include "vendor_init.h"
 
-static const variant_info_t lmi_info = {
+static const variant_info_t lmi_global_info = {
     .hwc_value = "",
     .sku_value = "std",
 
@@ -35,9 +35,23 @@ static const variant_info_t lmipro_info = {
     .nfc = true,
 };
 
+static const variant_info_t lmi_info = {
+    .hwc_value = "",
+    .sku_value = "",
+
+    .brand = "Redmi",
+    .device = "lmi",
+    .marketname = "",
+    .model = "Redmi K30 Pro",
+    .build_fingerprint = "Redmi/lmi/lmi:11/RKQ1.200826.002/V12.5.3.0.RJKMIXM:user/release-keys",
+
+    .nfc = true,
+};
+
 static const std::vector<variant_info_t> variants = {
-    lmi_info,
+    lmi_global_info,
     lmipro_info,
+    lmi_info,
 };
 
 void vendor_load_properties() {
