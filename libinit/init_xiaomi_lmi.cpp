83a7d555fa0db2249bc492fef144fa044577e544
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-11 01:28:51 +0100

    lmi: libinit: Update fingerprints to V12.5.3.0.RJKMIXM
    Change-Id: Ic1b0b42dad512561b9650d745a6bd65ab86398fe

--- libinit/init_xiaomi_lmi.cpp
+++ libinit/init_xiaomi_lmi.cpp
@@ -17,7 +17,7 @@ static const variant_info_t lmi_global_info = {
     .device = "lmi",
     .marketname = "",
     .model = "POCO F2 Pro",
-    .build_fingerprint = "POCO/lmi_global/lmi:11/RKQ1.200826.002/V12.5.2.0.RJKMIXM:user/release-keys",
+    .build_fingerprint = "POCO/lmi_global/lmi:11/RKQ1.200826.002/V12.5.3.0.RJKMIXM:user/release-keys",
 
     .nfc = true,
 };
@@ -30,7 +30,7 @@ static const variant_info_t lmipro_info = {
     .device = "lmipro",
     .marketname = "",
     .model = "Redmi K30 Pro Zoom Edition",
-    .build_fingerprint = "Redmi/lmipro/lmipro:11/RKQ1.200826.002/V12.5.2.0.RJKMIXM:user/release-keys",
+    .build_fingerprint = "Redmi/lmipro/lmipro:11/RKQ1.200826.002/V12.5.3.0.RJKMIXM:user/release-keys",
 
     .nfc = true,
 };
