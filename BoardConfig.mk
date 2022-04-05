0a4567bc0c06d2a5e35f9830031fb2f481c4d782
Bruno Martins <bgcngm@gmail.com>
2022-04-05 10:34:38 +0100

    umi: Inherit from QTI FCM
    Change-Id: I0bdd4d63b60a1e29cdd69e71f7268932a105c16b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -86,6 +86,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //$(DEVICE_PATH):libudfps_extension.umi
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/vintf/device_framework_matrix.xml \
     $(DEVICE_PATH)/vintf/xiaomi_framework_matrix.xml
 
