c6050f440832818dcd071c0b8c0d269fa41b5b87
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-21 21:51:41 +0800

    umi: Enable xiaomi powershare HAL

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,8 +24,16 @@ TARGET_KERNEL_CONFIG := vendor/umi_defconfig
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := umi
 
+# PowerShare
+SOONG_CONFIG_NAMESPACES += XIAOMI_POWERSHARE
+SOONG_CONFIG_XIAOMI_POWERSHARE := WIRELESS_TX_ENABLE_PATH
+SOONG_CONFIG_XIAOMI_POWERSHARE_WIRELESS_TX_ENABLE_PATH := /sys/devices/platform/soc/c440000.qcom,spmi/spmi-0/spmi0-02/c440000.qcom,spmi:qcom,pm8150b@2:qcom,qpnp-smb5/power_supply/wireless/reverse_chg_mode
+
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Inherit from the proprietary version
 include vendor/xiaomi/umi/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -38,6 +38,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml
 
+# PowerShare
+PRODUCT_PACKAGES += \
+    vendor.lineage.powershare@1.0-service.xiaomi
+
 # Rootdir
 PRODUCT_PACKAGES += \
     init.device.rc
