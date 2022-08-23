b8edab3c6c2cc241c26e399c23d655b63da5c35b
Josh Wu <joshwu@google.com>
2022-08-23 07:24:02 +0000

    umi: android.hardware.bluetooth.audio: Migrate to AIDL
    Signed-off-by: Bartłomiej Rudecki <ejbtrd@gmail.com>

--- device.mk
+++ device.mk
@@ -124,7 +124,7 @@ BOARD_HAVE_QCOM_FM := false
 
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.1.vendor \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
     com.dsi.ant@1.0.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
