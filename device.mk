72329c9e7fd6a6dbe313d81ee66689ca25cd6260
althafvly <althafvly@gmail.com>
2022-11-19 12:45:23 +0530

    umi: Add libpiex_shim for com.mi.node.watermark.so
    Change-Id: I9e8b125ab5dfaa325db63b02a7fcf51577ee7b0d

--- device.mk
+++ device.mk
@@ -25,7 +25,8 @@ TARGET_SCREEN_WIDTH := 1080
 # Camera
 PRODUCT_PACKAGES += \
     libMegviiFacepp-0.5.2 \
-    libmegface
+    libmegface \
+    libpiex_shim
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
