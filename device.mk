64e560e677c4ce33f94826c47c488e0c7e14ade9
Bartłomiej Rudecki <ejbtrd@gmail.com>
2022-11-21 17:35:28 +0100

    umi: Switch to android.hidl.base from hardware/tequila/compat

--- device.mk
+++ device.mk
@@ -191,8 +191,6 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0 \
-    android.hidl.base@1.0.vendor \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
