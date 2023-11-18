36417c562297cbf7dca0a91e578f088ac051f1a3
Bartłomiej Rudecki <ejbtrd@gmail.com>
2023-11-18 12:47:02 +0100

    umi: Time for tequila!
    Change-Id: I833ca16c8e23ee0a3b5f79f4f0479875ae1f6d52

--- device.mk
+++ device.mk
@@ -31,7 +31,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-tequila
 
 PRODUCT_PACKAGES += \
     xiaomi_umi_hwlevel_default_overlay \
