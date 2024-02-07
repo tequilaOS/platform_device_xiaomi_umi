db7c077308a0eb0b0364af35ee8cfbddf1399c04
Yumi Yukimura <me.cafebabe@gmail.com>
2024-02-07 22:02:43 +0800

    umi: Update blobs to V14.0.5.0.TJBMIXM
    Change-Id: Ic2f96017001e3f304b07f1bdddbece10a7f4e68f

--- libinit/init_xiaomi_umi.cpp
+++ libinit/init_xiaomi_umi.cpp
@@ -17,7 +17,7 @@ static const variant_info_t umi_info = {
     .device = "umi",
     .marketname = "",
     .model = "Mi 10",
-    .build_fingerprint = "Xiaomi/umi/umi:12/RKQ1.211001.001/V13.0.6.0.SJBCNXM:user/release-keys",
+    .build_fingerprint = "Xiaomi/umi_global/umi:13/RKQ1.211001.001/V14.0.5.0.TJBMIXM:user/release-keys",
 
     .nfc = true,
 };
